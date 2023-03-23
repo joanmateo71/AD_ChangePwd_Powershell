ceae0b2434a90ac54d2a8f3873861d365944d8be
Joan Mateo <88201505+joanmateo71@users.noreply.github.com>
2023-03-23 18:40:31 +0100

    test

--- ScriptPwd.ps1
+++ ScriptPwd.ps1
@@ -1,1 +1,10 @@
 # I'm writing the code, pls wait
+echo "Welcome to ADchangePwd."
+
+$rsatAD = Get-WindowsFeature | Where {$_.Name -eq "RSAT-AD-Tools"}
+if($rsatAD.Installed -eq "False"){
+    echo "RSAT Tools are not installed on the computer. Proceeding to install it."
+    Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability –Online    
+    }
+
+echo "Copy and paste the route "
